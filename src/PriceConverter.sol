bca39134b9604ea46789e73b899aaeb0179f9250
drikssy <cedricnga2@gmail.com>
2024-04-03 14:27:27 +0200

    price Converter update

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -6,12 +6,14 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 // Why is this a library and not abstract?
 // Why not an interface?
 abstract contract PriceConverter {
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
     // We could make this public, but then we'd have to deploy it
+
     function _getPrice(address priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
         (, int256 answer,,,) = AggregatorV3Interface(priceFeed).latestRoundData();
         // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+        return uint256(answer) * ADDITIONAL_FEED_PRECISION;
     }
 }
